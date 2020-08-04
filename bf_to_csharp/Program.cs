e26be95182560aa0f902cd0808fcec412fbd55cf
David Betteridge <david.betteridge@proactis.com>
2020-08-04 23:36:57 +0100

    IL prep

--- bf_to_csharp/Program.cs
+++ bf_to_csharp/Program.cs
@@ -2,9 +2,9 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 namespace bf_to_csharp
 {
     class Program
@@ -27,6 +27,7 @@ namespace bf_to_csharp
             var parentFolder = Path.GetDirectoryName(sourceCodeFile);
             var projectName = Path.GetFileNameWithoutExtension(sourceCodeFile);
             var projectFolder = Path.Combine(parentFolder, projectName);
+            
             Directory.CreateDirectory(projectFolder);
 
             var sourceCode = File.ReadAllText(sourceCodeFile);
@@ -36,6 +37,63 @@ namespace bf_to_csharp
             rootBlock = Optimiser.Optimise(rootBlock, null);
 
             GenerateCSharp(projectName, projectFolder, rootBlock);
+
+            var fileName = Path.Combine(projectFolder, projectName + ".exe");
+            var assemblyNameDefinition = new AssemblyNameDefinition(projectName, new Version(1, 0, 0, 0));
+            using (var assemblyDefinition = AssemblyDefinition.CreateAssembly(assemblyNameDefinition, projectName, ModuleKind.Console))
+            {
+                var mainModule = assemblyDefinition.MainModule;
+
+                var pathToSystemConsole = @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\3.1.0\ref\netcoreapp3.1\System.Console.dll";
+                var systemConsoleAssembly = AssemblyDefinition.ReadAssembly(pathToSystemConsole);
+
+                var pathToSystemRuntime = @"C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\3.1.0\ref\netcoreapp3.1\System.Runtime.dll";
+                var systemRuntimeAssembly = AssemblyDefinition.ReadAssembly(pathToSystemRuntime);
+
+                var systemObjectType = systemRuntimeAssembly.Modules
+                                      .SelectMany(m => m.Types)
+                                      .Where(t => t.FullName == "System.Object")
+                                      .SingleOrDefault();
+                var systemObjectRef = assemblyDefinition.MainModule.ImportReference(systemObjectType);
+
+                var voidType = systemRuntimeAssembly.Modules
+                      .SelectMany(m => m.Types)
+                      .Where(t => t.FullName == "System.Void")
+                      .SingleOrDefault();
+                var voidTypeRef = assemblyDefinition.MainModule.ImportReference(voidType);
+
+                var systemConsoleType = systemConsoleAssembly.Modules
+                                                      .SelectMany(m => m.Types)
+                                                      .Where(t => t.FullName == "System.Console")
+                                                      .SingleOrDefault();
+                assemblyDefinition.MainModule.ImportReference(systemConsoleType);
+                var writeLineMethod = systemConsoleType.Methods.SingleOrDefault(m => m.Name == "WriteLine" && m.Parameters.Count == 1 && m.Parameters[0].ParameterType.FullName == "System.String");
+                var writeLine = assemblyDefinition.MainModule.ImportReference(writeLineMethod);
+
+                var type = new TypeDefinition("app", "Program", TypeAttributes.NotPublic | TypeAttributes.Sealed, systemObjectRef)
+                {
+                    IsBeforeFieldInit = true
+                };
+                var main = new MethodDefinition("Main", MethodAttributes.Private | MethodAttributes.Static, voidTypeRef);
+
+               // var writeLine = mainModule.ImportReference(typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
+
+                var il = main.Body.GetILProcessor();
+                il.Emit(OpCodes.Nop);
+                il.Emit(OpCodes.Ldstr, "Hello, Mate!");
+                il.Emit(OpCodes.Call, writeLine);
+                il.Emit(OpCodes.Nop);
+                il.Emit(OpCodes.Ret);
+
+                type.Methods.Add(main);
+                mainModule.Types.Add(type);
+
+
+                mainModule.EntryPoint = main;
+
+                assemblyDefinition.EntryPoint = main;
+                assemblyDefinition.Write(fileName);
+            }
 
         }
 
@@ -116,7 +174,7 @@ namespace bf_to_csharp
 
         private static string ReadTemplate(string templateName)
         {
-            var assembly = Assembly.GetExecutingAssembly();
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
             var resourceName = "bf_to_csharp.Templates." + templateName;
 
             using var stream = assembly.GetManifestResourceStream(resourceName);
